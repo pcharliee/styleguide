dc96f09af4a4045da4d33166f1abefd30907bc14
Carlos Delgado <carlosm.delgador@gmail.com>
2022-05-31 17:47:11 -0300

    Modified entry for defaults

--- dev/webpack.config.js
+++ dev/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: process.env.NODE_ENV || 'development',
-  entry: '../src/defaults.js',
+  entry: '../../src/defaults.js',
   devtool: false,
   output: {
     path: path.join(__dirname, '../../../src'),
